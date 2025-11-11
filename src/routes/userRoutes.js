98f3529d325123be7014db7b73babc8671f3dfa2
Andrew <bakarekeandrew@gmail.com>
2025-11-12 01:13:20 +0200

    created the messageroute

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -6,21 +6,22 @@ const router = express.Router();
 router.get('/', async(req, res) => {
   try{
       const userData = await User.find({});
-      res.json("Data retrieved here!")
       res.status(200).json(userData)
+      res.json("Data retrieved here!")
   }
-  catch(error){
-     res.status(501).json(error)
+  catch (error) {
+    console.error("Error while adding user:", error); 
+    res.status(500).json({ message: "Server Error", error: error.message });
   }
 })
 //post
  router.post('/add', async(req, res)=>{
     try{
-      const add = await User.create(req.body.title)
+      const add = await User.create({username:req.body.username})
       res.status(200).json("Username added successfully!")
     }
     catch(error){
-      res.status(501).json(error)
+      res.status(500).json(message.error)
     }
  })
 
