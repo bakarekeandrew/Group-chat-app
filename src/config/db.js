0c2e87e48e0f8650bb5a2b5e61b84249d1bc01e2
Andrew <bakarekeandrew@gmail.com>
2025-11-11 20:04:35 +0200

    added database connection

--- src/config/db.js
+++ src/config/db.js
@@ -1,19 +1,12 @@
-const mongoose = require('mongoose');
+const mongoose = require('mongoose')
 
-const connectDB = async () => {
-  try {
-    await mongoose.connect(
-      "mongodb+srv://luckybrownandrew_db_user:123@chat.khvzzqd.mongodb.net/chat-app?retryWrites=true&w=majority&appName=Chat",
-      {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-      }
-    );
-    console.log("Database connected successfully");
-  } catch (error) {
-    console.error("MongoDB failed to connect:", error.message);
-    process.exit(1);
-  }
-};
 
-module.exports = connectDB;
+mongoose.connect("mongodb+srv://luckybrownandrew_db_user:123@cluster0.xubiwuh.mongodb.net/?appName=Cluster0")
+.then(()=>{
+    console.log("Database Connected!")
+})
+.catch((error)=>{
+    console.error("Database connection fail", error)
+})
+
+mongoose.exports = mongoose
--- src/models/User.js
+++ src/models/User.js
@@ -3,10 +3,10 @@ const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
     username:{
-       type: String,
+       type: String,                    
        require: true,
        unique: true
     }
 },{timeStamps: true});
 
-module.exports = mongoose.model('User',userSchema);
+module.exports = mongoose.model('User',userSchema);  
