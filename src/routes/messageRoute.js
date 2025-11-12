8e27c8ef642048d1b1865987761d0d6b9830a893
Andrew <bakarekeandrew@gmail.com>
2025-11-12 13:47:19 +0200

    added the get route for message

--- src/routes/messageRoute.js
+++ src/routes/messageRoute.js
@@ -3,6 +3,12 @@ const router = require(router)
 
 
 //get
-// router.get('/', async(req,res)=>{
-//     const message = 
-// }) 
+router.get('/', async(req,res)=>{
+    try{
+       const message = await User.find({})
+       res.status(200).json("Data received successful")
+    }catch(error){
+        res.status(500).json({message: message.error})
+    }
+    
+}) 
