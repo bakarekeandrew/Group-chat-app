9edf94d607bdcac26ac8904ea9d9d3e147bda88d
Andrew <bakarekeandrew@gmail.com>
2025-11-13 13:45:55 +0200

    added the POST  method implementation

--- src/routes/messageRoute.js
+++ src/routes/messageRoute.js
@@ -5,12 +5,14 @@ const Message = require('../models/message')
 //get
 router.get('/', async(req,res)=>{
     try{
-       const message = await Message.find({})
-       res.status(200).json("Data received successful")
-    }catch(error){
-        res.status(500).json({message: message.error})
+       const messages = await Message.find({})
+       .populate('sender', 'username')
+       .populate('receiver', 'username')
+       res.status(200).json(messages)
     }
-    
+    catch(error){
+        res.status(500).json({message:error})
+    }
 }) 
 
 //post
@@ -27,9 +29,34 @@ router.post('/add', async(req,res)=>{
     res.status(200).json(message)
 
     }catch(error){
-        res.status(500).json("Data failed to be saved")
+        console.error(error)
+        res.status(500).json({message:"Data failed to be saved", error: error.message})
     }
 })
+router.put('/edit/:id', async(req,res)=>{
+    const { id } = req.params
+    const { text } = req.body
+    const updatedMessage = await Message.findByIdAndUpdate(
+        id,
+        { text },
+        { new:true }
+    );
+    if(!updatedMessage){
+        res.status(404).json("message doesn't found")
+    }
+    res.status(200).json({message:"Message update successfully", data: updatedMessage});
+})
+
+//delete
+router.delete('/delete', async(req,res)=>{
+    id = req.params
+    const message = await Message.findByIdAndDelete(id)
+    if(!id){
+        res.status(404).json("Id not found")
+    }
+    res.status(200).json("Message deleted successfully")
+})
+
 
 
 module.exports = router
